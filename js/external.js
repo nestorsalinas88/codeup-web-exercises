2ba694ed67a7c2ce83da53aad5cd64686418f4d7
nestorsalinas88 <nestorasm88@gmail.com>
2018-03-19 10:41:11 -0500

    created my inline and external JS scripts

---
+++ js/external.js
@@ -0,0 +1,5 @@
+"use strict";
+console.log("Hello from external JavaScript");
+alert("Welcome to my Website!");
+var userInput = prompt("What is your favorite color?");
+    console.log("Great, " + userInput + " is my favorite color too!");
