720ac5b7cb46ee0349c04fc6104d719e79d092b7
nestorsalinas88 <nestorasm88@gmail.com>
2018-03-19 11:08:31 -0500

    refactored alert for console.log

--- js/external.js
+++ js/external.js
@@ -2,4 +2,4 @@
 console.log("Hello from external JavaScript");
 alert("Welcome to my Website!");
 var userInput = prompt("What is your favorite color?");
-    console.log("Great, " + userInput + " is my favorite color too!");
+    alert("Great, " + userInput + " is my favorite color too!");
