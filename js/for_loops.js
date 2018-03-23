4602099c7cdf03d5f2d90698f0d6199eaad602bd
nestorsalinas88 <nestorasm88@gmail.com>
2018-03-23 14:52:30 -0500

    completed for loops exercise

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,31 +1,60 @@
 "use strict";
 
-//1.create function that accepts a number
-//2.function must console.log the multiplication table for that number
-//3. multiplication should be from 1 - 10
+///// FUNCTIONS ARE SUPPOSE TO HELP YOU SIMPLIFY NOT MAKE HARDER
 
-//////////////STOP MAKING IT COMPLICATED BRUHHH ///////////////
+//we know when the loop needs to end so we use a for loop
+// function showMultiplicationTable(x) {
+//     for(var i = 1; i <=10; i++){
+//         console.log(x + " X " + i + " = " + (x * i));
 //
-// function showMultiplicationTable(number) {
-//    var factor = parseFloat(number);
-//    var factory = Math.floor(Math.random() * 10) + 1;
-//    var product = (factor * factory);
-//    console.log(factor + "X" + factory + "=" + product);
+//     }
 //
 // }
+// //for loops need a initializer, condition, and increment statements
+//
+// showMultiplicationTable(7);
+//
+//
+// //
+// // //if new random needs to be selected every time needs to be with in the function
+// // //local and global
+// //
+// //
+//
+// //when generating random numbers you should initialize with the middle value and add up to where you'd like to stop
+//
+// function randomInt(start, finish) {
+//     return Math.floor(Math.random() * (end - start)) + start;
+// }
+//
+//
+//
+// for (var i = 1; i <= 10; i++) {
+//     var x = Math.floor(Math.random() * 180) + 20;
+//     if ((x % 2 === 0)) {
+//         console.log(x + " is even ");
+//     } else {
+//         console.log(x + " is odd");
+//     }
+// }
+//
 
-function showMultiplicationTable(x) {
-    for(var i = 1; i <=10; i++){
-        console.log("x * i);
 
-    }
 
+
+var string;
+for (var i = 1; i <=  10; i++){
+    i = i.toString();
+
+    string = i.substring(i.length - 1);
+
+
+    console.log(string.repeat(i));
 }
 
+//when using loops  try and keep all in initializer, codition, and increment statement section
 
-showMultiplicationTable(7);
+for(var i = 100; i >= 5; i -= 5){
 
-
-
-
-// var random = Math.floor(Math.random() * 100) + 20;
+    console.log(i);
+}
