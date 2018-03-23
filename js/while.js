65847e701ad9f978d6a3aadfcc67b1a44a4f0a46
nestorsalinas88 <nestorasm88@gmail.com>
2018-03-23 11:54:15 -0500

    Completed while and for loop exercise

--- js/while.js
+++ js/while.js
@@ -1,17 +1,15 @@
 "use strict";
 
-var i = 2;
-while( i <= 65536){
-    console.log(i);
-    i*=2
+//we're always multiplying the initial number by * 2
+var i = 2; /* initializer */
+//assigning a variable before
+while( i <= 65536 /*condition*/){
+    console.log(i);/*thing we're repeating */
+    i*=2 /* increment/decrement/accumulator*/
 }
 
 
 
-//1. generate a random number between 50 & 100
-//2. code should generate numbers between 1 - 5
-//3. use a do-while loop structure
-//4. is it login to console the amount of cones sold to each person
 
 var allCones = Math.floor(Math.random() * 50) + 50;
 console.log(allCones + ' cones to sell');
@@ -19,11 +17,11 @@ console.log(allCones + ' cones to sell');
 do {
     var cones = Math.floor(Math.random() * 5) + 1;
     console.log('I would like ' + cones + ' cones please!');
-    if(allCones < cones) {
+    if(allCones <= cones) {
         console.log("can't sell you " + cones + " I only have " + allCones);
         allCones -= allCones;
         console.log('my inventory says I have ' + allCones + ' left');
-        console.log("cones === 0, yay I am done");
+        console.log("Yay I am done");
     } else {
         allCones -= cones;
         console.log("you still have " + allCones +" left");
